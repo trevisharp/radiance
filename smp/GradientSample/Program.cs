a2074d5b01d86eef55c928cf6ea188b2e7c74975
trevisharp <leonardosilio@alunos.utfpr.edu.br>
2023-08-16 09:15:05 -0300

    Add propose to ColoredVector use

---
+++ smp/GradientSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Radiance;
+using static Radiance.RadianceUtils;
+
+var region = data(
+    (0, 0, 0) | (1, 0, 0),
+    (1, 0, 0) | (0, 0, 1),
+    (1, 1, 0) | (1, 0, 0),
+
+    (0, 0, 0) | (1, 0, 0),
+    (0, 1, 0) | (0, 0, 1),
+    (1, 1, 0) | (1, 0, 0)
+);
+
+Window.OnRender += r =>
+{   
+    r.FillTriangles(region
+        .transform((v, u) => ((width * v.x, height * v.y, v.z), u))
+        .colorize(u => u)
+    );
+};
+
+Window.CloseOn(Input.Escape);
+
+Window.Open();
