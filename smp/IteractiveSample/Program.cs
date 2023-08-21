0c3eef591dd55da5a1f1ecd25cc47f6cc6cf1c76
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-08-21 14:05:47 -0300

    Added Sample project for the feature

---
+++ smp/IteractiveSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using Radiance;
+using static Radiance.RadianceUtils;
+
+float x = 0;
+float y = 0;
+
+var region = data(n, i, i + j, j);
+
+Window.OnLoad += delegate
+{
+    x = Window.Width / 2 - 25;
+    y = Window.Height / 2 - 25;
+};
+
+Window.OnRender += r =>
+{
+    r.Draw(region
+        .transform(v => (v.x * 50 + x, v.y * 50 + y, v.z))
+    );
+};
+
+Window.OnKeyDown += input =>
+{
+    if (input == Input.D)
+        x += 5;
+    
+    if (input == Input.A)
+        x -= 5;
+    
+    if (input == Input.W)
+        y += 5;
+
+    if (input == Input.S)
+        y -= 5;
+};
+
+Window.CloseOn(Input.Escape);
+
+Window.Open();
