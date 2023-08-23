61be937daceb070d71a5dbb10b925ae272e5b59b
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-08-23 16:36:53 -0300

    Add sample to show buffer improvements

---
+++ smp/MutableSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Radiance;
+using static Radiance.RadianceUtils;
+
+var end = i + j; 
+
+var region = data(
+    n, i, end,
+    n, j, end
+);
+
+Window.OnRender += r =>
+{
+    r.Clear(black);
+
+    r.FillTriangles(region
+        .colorize(red)
+    );
+};
+
+Window.OnFrame += () =>
+{
+    end.x++;
+    end.y++;
+};
+
+Window.CloseOn(Input.Escape);
+
+Window.Open();
