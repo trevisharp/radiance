04e59caff0b4bb6392a5660f0de2682d5ae488c3
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-08-23 22:07:43 -0300

    Create project to show and test mult fill codes

---
+++ smp/MultiFillSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Radiance;
+using static Radiance.RadianceUtils;
+
+var rect = data(
+    n, i, i + j,
+    n, j, i + j
+);
+
+Window.OnRender += r =>
+{
+    r.Verbose = true;
+    for (int i = 0; i < 10; i++)
+    {
+        int j = i;
+        r.FillTriangles(rect
+            .transform(v => (v.x * 50 * (j + 1), v.y * 50 * (j + 1), 0))
+            .colorize(25 * j / 255f, 25 * j / 255f, 25 * j / 255f)
+        );
+    }
+};
+
+Window.CloseOn(Input.Escape);
+
+Window.Open();
