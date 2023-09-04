113f69c11f95ec0c46e261706b788e22e65c2e75
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-09-03 22:12:11 -0300

    Use Base Data in basic data types

--- src/Data/ColoredVector.cs
+++ src/Data/ColoredVector.cs
@@ -1,7 +1,6 @@
 /* Author:  Leonardo Trevisan Silio
- * Date:    23/08/2023
+ * Date:    03/09/2023
  */
-using System;
 using System.Collections.Generic;
 
 namespace Radiance.Data;
@@ -10,47 +9,27 @@ using ShaderSupport;
 using ShaderSupport.Objects;
 using ShaderSupport.Dependencies;
 
-public class ColoredVector : IData<Vec3ShaderObject, Vec4ShaderObject>
+public class ColoredVector : BaseData<Vec3ShaderObject, Vec4ShaderObject>
 {
     public Vector Vector { get; set; }
     public Color Color { get; set; }
 
-    public event Action OnChange;
-    public void HasChanged()
-    {
-        if (OnChange is null)
-            return;
-        
-        OnChange();
-    }
-
-    public Vec3ShaderObject VertexObject => Data1;
-    public Vec4ShaderObject FragmentObject => Data2;
-
-    public IEnumerable<ShaderOutput> Outputs 
+    public override Vec3ShaderObject VertexObject => Data1;
+    public override Vec4ShaderObject FragmentObject => Data2;
+    public override IEnumerable<ShaderOutput> Outputs 
         => ShaderOutput.Empty;
 
-    public int Size => 7;
-    public int Elements => 1;
-    public IEnumerable<int> Sizes => new int[] { 3, 4 };
+    public override int Size => 7;
+    public override int Elements => 1;
+    public override IEnumerable<int> Sizes => new int[] { 3, 4 };
 
-    public Vec3ShaderObject Data1
+    public override Vec3ShaderObject Data1
         => new PositionBufferDependence(this, 0);
     
-    public Vec4ShaderObject Data2
+    public override Vec4ShaderObject Data2
         => new ColorBufferDependence(this, 1);
 
-    public float[] GetBuffer()
-    {
-        float[] buffer = new float[this.Size];
-
-        int indexoff = 0;
-        this.SetData(buffer, ref indexoff);
-        
-        return buffer;
-    }
-
-    public void SetData(float[] arr, ref int indexoff)
+    public override void SetData(float[] arr, ref int indexoff)
     {
         this.Vector.SetData(arr, ref indexoff);
         this.Color.SetData(arr, ref indexoff);
