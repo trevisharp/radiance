effa38bb308b91a2cc743061f6fe0bb69f1f95b4
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-01-08 01:15:56 -0300

    update using new circle property feature

--- smp/TextureSample/Program.cs
+++ smp/TextureSample/Program.cs
@@ -6,7 +6,7 @@ var myRender = render(r =>
     var img = open("pain.png");
     var mod = 400 * sin(t / 5);
     r.Clear(white);
-    r.FillTriangles(ellip(0, 0, 1)
+    r.FillTriangles(circle
         .triangules()
         .transform(v => (mod * v.x + width / 2, mod * v.y + height / 2, v.z))
         .colorize(v => texture(img, (0.3, 0.3)))
