9f4ee672646c8c7a169013e40a0165716aac7002
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-01-08 21:16:41 -0300

    improve use of texture

--- smp/TextureSample/Program.cs
+++ smp/TextureSample/Program.cs
@@ -6,10 +6,11 @@ var myRender = render(r =>
     var img = open("pain.png");
     var mod = 400 * sin(t / 5);
     r.Clear(white);
+    r.Verbose = true;
     r.FillTriangles(circle
         .triangules()
         .transform(v => (mod * v.x + width / 2, mod * v.y + height / 2, v.z))
-        .colorize(v => texture(img, (0.3, 0.3)))
+        .colorize(v => texture(img, (mod * v.x / width + 0.5f, mod * v.y / height + 0.5f)))
     );
 });
 
