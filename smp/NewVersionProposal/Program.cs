3c9e39f87e19839e67a93b17b08fcbab9e875107
trevisharp <leonardotrevisansilio@gmail.com>
2024-01-18 11:49:16 -0300

    Add proposal of Radiance 2.0.0 usage

---
+++ smp/NewVersionProposal/Program.cs
@@ -0,0 +1,27 @@
+﻿using Radiance;
+using static Radiance.RadianceUtils;
+
+var myRender = render((cr, cg, cb, amp) =>
+{
+    x += width / 2;
+    y += height / 2;
+
+    x += amp * sin(t);  
+    y += amp * cos(t);
+
+    r = cr;
+    g = cg;
+    b = cb;
+
+    fill();
+});
+
+Polygon data = rect(50, 50);
+
+Window.OnRender += () =>
+{
+    myRender(data, red, 5, 50);
+};
+
+Window.CloseOn(Input.Escape);
+Window.Open();
