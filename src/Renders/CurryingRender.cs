9614d992e1c929b90a84a354c1bf40521ef0184d
trevisharp <leonardotrevisansilio@gmail.com>
2024-01-23 14:26:52 -0300

    Solve Currying bugs and enable CurryingRender to Action convertion

--- src/Renders/CurryingRender.cs
+++ src/Renders/CurryingRender.cs
@@ -4,6 +4,9 @@
 using System.Linq;
 using System.Dynamic;
 using System.Collections.Generic;
+using System;
+using Radiance.Exceptions;
+using System.ComponentModel;
 
 namespace Radiance.Renders;
 
@@ -37,6 +40,19 @@ public class CurryingRender : DynamicObject, ICurryable
     }
 
     public dynamic Curry(params object[] parameters)
-        => parent.Curry(parameters);
-
+        => new CurryingRender(parent, this.parameters.Concat(parameters).ToArray());
+    
+    public static implicit operator Action(CurryingRender render)
+    {
+        var expectedParams = render.parent.ExtraParameterCount + 1;
+        var actualParams = render.parameters.Count;
+        if (expectedParams < actualParams)
+            throw new SurplusParametersException();
+        
+        if (expectedParams > actualParams)
+            throw new MissingParametersException();
+        
+        return () => render.parent.TryInvoke(
+            null, render.parameters.ToArray(), out _);
+    }
 }
