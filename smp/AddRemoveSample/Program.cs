37fbe273c7a5dbe5e64d11b80b54b7c01296fa75
trevisharp <leonardotrevisansilio@gmail.com>
2024-01-23 14:33:02 -0300

    Test AddRemoveSample

--- smp/AddRemoveSample/Program.cs
+++ smp/AddRemoveSample/Program.cs
@@ -1,10 +1,15 @@
 ﻿using Radiance;
 using static Radiance.Utils;
 
+var myclear = render(() =>
+{
+    clear(black);
+});
+
 var render1 = render(() =>
 {
     pos += (width / 2, height / 2, 0);
-    var scale = (x - 50) / 50;
+    var scale = 0.8;//(x - 50) / 50;
     color = (scale, 0, 1, scale);
     fill();
 });
@@ -12,14 +17,14 @@ var render1 = render(() =>
 var render2 = render(() =>
 {
     pos += (width / 2, height / 2, 0);
-    var scale = (y - 100) / 50;
+    var scale = 0.8;//(y - 100) / 50;
     color = (0, scale, 1, scale);
     fill();
 });
 
 var rect = Rect(500, 500);
-var drawRect1 = render1(rect);
-var drawRect2 = render2(rect);
+Action drawRect1 = render1.Curry(rect);
+Action drawRect2 = render2.Curry(rect);
 
 Window.OnRender += drawRect1;
 Window.OnRender += drawRect2;
