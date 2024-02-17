419d5a78baeee7ae8ee53b938417c810b132fcbf
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-02-17 11:41:51 -0300

    make test with mult renders

---
+++ smp/MultiRenderSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using Radiance;
+using Radiance.Renders;
+using static Radiance.Utils;
+
+var renderA = render((dx, dy) =>
+{
+    pos += (dx, dy, 0);
+    color = white;
+    fill();
+});
+
+var renderB = render((dx, dy) =>
+{
+    pos += (dx, dy, 0);
+    color = white;
+    fill();
+});
+
+var rect = Rect(100, 100);
+Window.OnRender += () => 
+{
+    renderA(rect, 0, 0);
+    renderB(rect, 100, 100);
+};
+
+Window.CursorVisible = false;
+
+Window.CloseOn(Input.Escape);
+Window.Open();
