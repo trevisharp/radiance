b6c22140f07a57fd16e066324d29ad285948d37e
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-02-26 08:57:01 -0300

    add polygon collection experssion

--- src/Data/Polygon.cs
+++ src/Data/Polygon.cs
@@ -1,9 +1,11 @@
 /* Author:  Leonardo Trevisan Silio
  * Date:    26/02/2024
  */
+using System;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Radiance.Data;
 
@@ -12,8 +14,32 @@ using Internal;
 /// <summary>
 /// A base type for all polygons.
 /// </summary>
+[CollectionBuilder(typeof(PolygonBuilder), "Create")]
 public abstract class Polygon : IEnumerable<float>
 {
+    internal static class PolygonBuilder
+    {
+        internal static Polygon Create(ReadOnlySpan<float> values)
+        {
+            var polygon = new MutablePolygon();
+            var it = values.GetEnumerator();
+            while (it.MoveNext())
+            {
+                float x = it.Current;
+                if (!it.MoveNext())
+                    polygon.add(x, 0, 0);
+                
+                float y = it.Current;
+                if (!it.MoveNext())
+                    polygon.add(x, y, 0);
+                
+                float z = it.Current;
+                polygon.add(x, y, z);
+            }
+            return polygon;
+        }
+    }
+
     private MutablePolygon triangulationPair = null;
 
     /// <summary>
@@ -73,7 +99,15 @@ public abstract class Polygon : IEnumerable<float>
     /// <summary>
     /// Add a point in polygon.
     /// </summary>
-    public abstract Polygon Add(float x, float y, float z);
+    public Polygon Add(float x, float y, float z)
+    {
+        add(x, y, z);
+        if (OnChange is not null)
+            OnChange(true, true);
+        return this;
+    }
+
+    protected abstract void add(float x, float y, float z);
     
     /// <summary>
     /// Create a copy of this polygon.
@@ -85,6 +119,11 @@ public abstract class Polygon : IEnumerable<float>
     /// </summary>
     public virtual Polygon ToImmutable()
         => new ImmutablePolygon(Data);
+
+    /// <summary>
+    /// Event called when data is changed.
+    /// </summary>
+    public Action<bool, bool> OnChange;
 
     public IEnumerator<float> GetEnumerator()
         => Data.GetEnumerator();
