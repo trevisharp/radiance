1fd88530159b533e6ea52263f5898fff732b009c
Trevisharp <leonardotrevisansilio@gmail.com>
2024-08-30 17:53:07 -0300

    Start to move and refactor Render abstraction

---
+++ src/Renders/Render.cs
@@ -0,0 +1,25 @@
+/* Author:  Leonardo Trevisan Silio
+ * Date:    30/08/2024
+ */
+using System;
+using System.Linq;
+using System.Dynamic;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Radiance.Renders;
+
+using Data;
+using Exceptions;
+
+using Shaders;
+using Shaders.Objects;
+using Shaders.Dependencies;
+
+/// <summary>
+/// Represents a function that can used by GPU to draw in the screen.
+/// </summary>
+public class Render
+{
+
+}
