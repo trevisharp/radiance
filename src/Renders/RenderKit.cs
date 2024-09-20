9d0d177b49357287b5d762c75f939f8987168f5b
trevisharp <leonardotrevisansilio@gmail.com>
2024-09-20 16:57:00 -0300

    fix typo

--- src/Renders/RenderKit.cs
+++ src/Renders/RenderKit.cs
@@ -4,7 +4,6 @@
 namespace Radiance.Renders;
 
 using System.Runtime.InteropServices;
-using Radiance.Shaders.Objects;
 using static Radiance.Utils;
 
 /// <summary>
@@ -14,63 +13,63 @@ public class RenderKit
 {
     public readonly static RenderKit Shared = new();
 
-    private dynamic? _fill;
+    private dynamic? fillRender;
     /// <summary>
     /// Receiving rgba color, fill a polygon.
     /// </summary>
     public void Fill(dynamic r, dynamic g, dynamic b, dynamic a)
     {
-        _fill ??= render((r, g, b, a) =>
+        fillRender ??= render((r, g, b, a) =>
         {
             color = (r, g, b, a);
             fill();
         });
 
-        _fill(r, g, b, a);
+        fillRender(r, g, b, a);
     }
 
-    private dynamic? _draw;
+    private dynamic? drawRender;
     /// <summary>
     /// Receiving rgba color, draw a polygon.
     /// </summary>
     public void Draw(dynamic r, dynamic g, dynamic b, dynamic a)
     {
-        _draw ??= render((r, g, b, a) =>
+        drawRender ??= render((r, g, b, a) =>
         {
             color = (r, g, b, a);
             draw();
         });
 
-        _draw(r, g, b, a);
+        drawRender(r, g, b, a);
     }
 
-    private dynamic? centralize;
+    private dynamic? centralizeRender;
     /// <summary>
     /// Centralize a polygon on the center of the screen.
     /// This render cannot perform draw/fill, consider using inside another shader.
     /// </summary>
     public void Centralize()
     {
-        centralize ??= render(() => {
+        centralizeRender ??= render(() => {
             pos += (width / 2, height / 2, 0);
         });
 
-        centralize();
+        centralizeRender();
     }
 
-    private dynamic? zoom;
+    private dynamic? zoomRender;
     /// <summary>
     /// Receiving x, y and a factor, performa a zoom on polygon on point (x, y) with the factor scale.
     /// This render cannot perform draw/fill, consider using inside another shader.
     /// </summary>
     public void Zoom(dynamic x, dynamic y, dynamic factor)
     {
-        zoom ??= render((cx, cy, factor) => {
+        zoomRender ??= render((cx, cy, factor) => {
             var nx = factor * (pos.x - cx) + cx;
             var ny = factor * (pos.y - cy) + cy;
             pos = (nx, ny, pos.z);
         });
 
-        zoom(x, y, factor);
+        zoomRender(x, y, factor);
     }
 }
