8c7a7a6586046c3f7c7527a44cc937163409eafe
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-11-08 18:22:27 -0300

    improve error message of unimplemented vulkan 1.3 version

--- src/Implementations/Vulkan13/Vulkan13Factory.cs
+++ src/Implementations/Vulkan13/Vulkan13Factory.cs
@@ -15,21 +15,21 @@ public class Vulkan13Factory : IImplementationFactory
     public ShaderContext NewContext()
     {
         throw new System.NotImplementedException(
-            "The implementation of Vulkan 1.3 not exists on this version of Radiance."
+            "Vulkan 1.3 implementation is missing in this version of Radiance."
         );
     }
 
     public BaseWindow NewWindow()
     {
         throw new System.NotImplementedException(
-            "The implementation of Vulkan 1.3 not exists on this version of Radiance."
+            "Vulkan 1.3 implementation is missing in this version of Radiance."
         );
     }
 
     public ICodeGenerator NewCodeGenerator()
     {
         throw new System.NotImplementedException(
-            "The implementation of Vulkan 1.3 not exists on this version of Radiance."
+            "Vulkan 1.3 implementation is missing in this version of Radiance."
         );
     }
 }
