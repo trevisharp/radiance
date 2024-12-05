b2906e13e6b262e733dba267233a60d5552fb91e
trevisharp <leonardotrevisansilio@gmail.com>
2024-12-05 11:27:48 -0300

    improve test example

--- smp/WavesSample/Program.cs
+++ smp/WavesSample/Program.cs
@@ -61,7 +61,7 @@ using System;
 //     Clock.Shared.ToogleFreeze();
 //     clkWave.ToogleFreeze();
 // };
-const int N = 8;
+const int N = 32;
 
 var mr = render((dx, dy, rdx, rdy, r, g, b) => {
     color = (r, g, b, 1);
@@ -72,26 +72,29 @@ var mr = render((dx, dy, rdx, rdy, r, g, b) => {
         const float dt = 0.4f;
         b.Wait(2 * dt);
 
+        var sum = 1.5 * (dx + dy);
+        var dif = 1.5 * (dx - dy);
+
         b.Step(dt, a => move(a * dx / 2, a * dy / 2));
-        b.Step(dt, a => rotate(-Math.PI / 2 * a));
+        b.Step(dt, a => move(-a * dif, -a * sum));
         b.Step(dt, a => move(-a * dy / 2, a * dx / 2));
 
         b.Wait(2 * dt);
 
         b.Step(dt, a => move(a * dy / 2, -a * dx / 2));
-        b.Step(dt, a => rotate(-Math.PI / 2 * a));
+        b.Step(dt, a => move(-a * sum, a * dif));
         b.Step(dt, a => move(a * dx / 2, a * dy / 2));
 
         b.Wait(2 * dt);
         
         b.Step(dt, a => move(-a * dx / 2, -a * dy / 2));
-        b.Step(dt, a => rotate(-Math.PI / 2 * a));
+        b.Step(dt, a => move(a * dif, a * sum));
         b.Step(dt, a => move(a * dy / 2, -a * dx / 2));
 
         b.Wait(2 * dt);
 
         b.Step(dt, a => move(-a * dy / 2, a * dx / 2));
-        b.Step(dt, a => rotate(-Math.PI / 2 * a));
+        b.Step(dt, a => move(a * sum, -a * dif));
         b.Step(dt, a => move(-a * dx / 2, -a * dy / 2));
 
         b.Loop();
