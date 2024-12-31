6ac589576cb67ee40f5a48a929a0f6f1a087ed40
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-12-31 16:31:06 -0300

    create a function to convert HSV to RGB

--- Radiance/Color.cs
+++ Radiance/Color.cs
@@ -1,12 +1,12 @@
 /* Author:  Leonardo Trevisan Silio
  * Date:    31/12/2024
  */
+using System;
 
 namespace Radiance;
 
 using Colors;
 using Primitives;
-using Exceptions;
 
 public static class Color
 {
@@ -15,6 +15,26 @@ public static class Color
     /// </summary>
     public static HSV CreateHSV(float h, float s, float v)
         => new(h, s, v);
-
     
+    /// <summary>
+    /// Convert a color for to RGB.
+    /// </summary>
+    public static Vec3 ToRGB(HSV color)
+    {
+        float C = color.V * color.S;
+        float X = C * (1 - MathF.Floor(color.H / 60 % 2 - 1));
+        float m = color.V - C;
+
+        Vec3 temp = color.H switch
+        {
+            >= 0 and < 60 => (C, X, 0),
+            >= 60 and < 120 => (X, C, 0),
+            >= 120 and < 180 => (0, C, X),
+            >= 180 and < 240 => (0, X, C),
+            >= 240 and < 300 => (X, 0, C),
+            _ => (C, 0, X),
+        };
+
+        return temp + (m, m, m);
+    }
 }
