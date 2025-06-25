f083b72f148771fb190e25df947431b7532e4453
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2025-06-25 00:39:11 -0300

    create the TrueTypeFontReader class

---
+++ Radiance/Fonts/TrueTypeFontReader.cs
@@ -0,0 +1,12 @@
+/* Author:  Leonardo Trevisan Silio
+ * Date:    25/06/2025
+ */
+namespace Radiance.Fonts;
+
+/// <summary>
+/// A reader for True Type Font (.fft) files.
+/// </summary>
+public class TrueTypeFontReader
+{
+    
+}
